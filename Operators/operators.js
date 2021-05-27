25df2e8f2f151f9121b7a74d9dcca272906dbb08
Sarah-khan26 <sarahkhan2692@gmail.com>
2021-05-27 12:50:03 -0700

    'jsPractice'

---
+++ Operators/operators.js
@@ -0,0 +1,59 @@
+//logical operators
+//&& and
+var a =5;
+var b =10;
+
+if(a==5 && b==10)
+{
+    alert('true')
+}
+else{
+    alert("false")
+}
+
+// ! (not)
+
+var g =true;
+var h =false;
+
+console.log(!g);
+console.log(!h);
+
+//|| or
+var c=15;
+var d=20;
+if (c==15 || d==25){
+    alert("true")
+}
+{
+    alert("false")
+}
+
+//assignment operator
+
+//+=
+
+x +=5;
+
+console.log(x)
+
+//=
+
+var x=10;
+
+//*=
+
+console.log(x);
+
+//  /=
+
+x /= 2;
+
+// %=
+
+x %=2;
+
+// **=
+
+x **=2;
+comsole.log(x);
--- Tempurature conversion/Temperature.js
+++
@@ -1,5 +0,0 @@
-function temperatureConverter(){
-    let valNum=parseFloat(valNum)
-    
-    document.getElementById(outputcelsius).innerHTML=(valNum-32)/1.8;
-}
